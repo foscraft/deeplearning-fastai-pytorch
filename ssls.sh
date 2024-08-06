ec284ca90bdd4a6cabe1637c485c562d02d40cdf
foscraft <anyaribari@gmail.com>
2024-08-06 15:32:43 +0300

    ssl-certificate renewal

--- ssls.sh
+++ ssls.sh
@@ -4,7 +4,7 @@
 domains=("ims.co.ke" "payments.ims.co.ke")
 
 for domain in "${domains[@]}"; do
-  echo "Checking SSL certificate for $domain..."
-  expiry_date=$(echo | openssl s_client -connect "$domain:443" -servername "$domain" 2>/dev/null | openssl x509 -noout -dates | grep 'notAfter' | cut -d= -f2)
-  echo "$domain expires on $expiry_date"
+    echo "Checking SSL certificate for $domain..."
+    expiry_date=$(echo | openssl s_client -connect "$domain:443" -servername "$domain" 2>/dev/null | openssl x509 -noout -dates | grep 'notAfter' | cut -d= -f2)
+    echo "$domain expires on $expiry_date"
 done
